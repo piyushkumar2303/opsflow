4eb0ab64620ab7b8ea093fada920b8eec147d9b8
Piyush Kumar <piyushkumar230301@gmail.com>
2026-09-22 09:23:39 +0530

    Replace sample endpoint with system status API

---
+++ src/OpsFlow.Api/Controllers/SystemController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpsFlow.Api.Controllers;
+
+[ApiController]
+[Route("api/system")]
+public class SystemController : ControllerBase
+{
+    [HttpGet("status")]
+    public IActionResult GetStatus()
+    {
+        return Ok(new
+        {
+            Service = "OpsFlow.Api",
+            Status = "Running"
+        });
+    }
+}
