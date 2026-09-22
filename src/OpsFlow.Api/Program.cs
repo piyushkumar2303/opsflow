5e5c2547b8a42d0e85ab156c1feb1ea0425ab696
Piyush Kumar <piyushkumar230301@gmail.com>
2026-09-22 10:07:30 +0530

    Add Swagger UI for API documentation

--- src/OpsFlow.Api/Program.cs
+++ src/OpsFlow.Api/Program.cs
@@ -1,9 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -12,6 +10,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "OpsFlow API v1");
+    });
 }
 
 app.UseHttpsRedirection();
